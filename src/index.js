28f69e4a6fbf42314c64c01d3fec494764efbaa8
foster1373 <foster.1373@osu.edu>
2025-03-31 23:12:58 -0400

    added vercel analytics

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import { createRoot } from 'react-dom/client'
 import { ChakraProvider } from '@chakra-ui/react'
 import App from './App'
 import theme from './theme'
+import { Analytics } from "@vercel/analytics/react"
+
 
 const container = document.getElementById('root')
 const root = createRoot(container)
@@ -11,6 +13,7 @@ const root = createRoot(container)
 root.render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
+      <Analytics />
       <App />
     </ChakraProvider>
   </React.StrictMode>
