dec4f769fc744da2b8bef816d72fc27aee7fb209
foster1373 <foster.1373@osu.edu>
2025-03-31 23:23:38 -0400

    added analytics

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -4,7 +4,7 @@ const About = () => (
   <Box p={8}>
     <Heading>About KODA</Heading>
     <Text mt={4}>
-    KODA is a high-energy rock band shaking up the Columbus music scene with blues-driven riffs, powerhouse vocals, and an electrifying stage presence. Formed in 2021, the band features lead guitarist Jake Klausner, rhythm guitarist/keyboardist Jackson Buss, bassist Jake Brewer, drummer Brian Foster, and vocalists Jake Juenger and Nolan Barber. Longtime friends Klausner, Buss, and Brewer met Juenger at a Miami University party, sparking an instant musical connection.
+    KODA is a high-energy rock band shaking up the Columbus music scene with blues-driven riffs, powerhouse vocals, and an electrifying stage presence. Formed in 2021, the band features lead guitarist Jake Klausner, rhythm guitarist/keyboardist Jackson Buss, bassist Jake Brewer, drummer Brian Foster, and vocalists Jake Juenger and Nolan Barbre. Longtime friends Klausner, Buss, and Brewer met Juenger at a Miami University party, sparking an instant musical connection.
     </Text>
     <Text mt={4}>
 Cutting their teeth on college campuses and bars, KODA quickly built a reputation for their explosive live shows. The bands chemistry has led to many breakthrough performances on the Columbus music scene, cementing themselves as one of Columbus’s more exciting rising acts. Inspired by bands like Led Zeppelin, Greta Van Fleet, and The Black Keys, KODA blends classic rock intensity with modern sounds.
